46a679c7543f94702f619961b58fb3b22c964e28
Zinex06 <gabrielwaadeeriksen@gmail.com>
2025-09-16 13:23:10 +0200

    test

--- password-generator-c#/password-generator-c#/obj/Debug/net8.0/password-generator-c#.AssemblyInfo.cs
+++ password-generator-c#/password-generator-c#/obj/Debug/net8.0/password-generator-c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("password-generator-c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21f88e775bf3688650174c87d025ea70de174e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("password-generator-c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("password-generator-c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
